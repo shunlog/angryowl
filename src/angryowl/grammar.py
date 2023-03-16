21461fd7310f7627a4a4d18c198f006d02645760
shunlog <artiombalan331@gmail.com>
2023-03-16 23:10:19 +0200

    finish documenting the grammar file

--- src/angryowl/grammar.py
+++ src/angryowl/grammar.py
@@ -2,12 +2,16 @@ from enum import IntEnum
 
 class Grammar:
     '''
-    A grammar is represented by 4 variables:
+    A `formal grammar <https://en.wikipedia.org/wiki/Formal_grammar#Formal_definition>`_
+    is defined by 4 components:
 
     :param VN: set of nonterminals
     :param VT: set of terminals
-    :param P: list of productions.
+    :param P: list of productions
     :param S: starting state
+
+    The list of productions is represented by a dictionary,
+    each rule being a mapping of a string of symbols onto another string of symbols.
 
     For example, the formal grammar::
 
@@ -27,17 +31,18 @@ class Grammar:
         S = "A"
     '''
 
-    Rule = tuple[str]
+    SymbolsStr = tuple[str]
 
     class Type(IntEnum):
-        '''Grammar classes according to the `Chomsky hierarchy <https://en.wikipedia.org/wiki/Chomsky_hierarchy>`_.
+        '''Grammar classes according to the `Chomsky hierarchy
+        <https://en.wikipedia.org/wiki/Chomsky_hierarchy>`_.
         '''
         UNRESTRICTED_GRAMMAR = 0
         CONTEXT_SENSITIVE = 1
         CONTEXT_FREE = 2
         REGULAR = 3
 
-    def __init__(self, VN: set[str], VT: set[str], P: dict[Rule, set[Rule]], S: str):
+    def __init__(self, VN: set[str], VT: set[str], P: dict[SymbolsStr, set[SymbolsStr]], S: str):
         self.VN = VN
         self.VT = VT
         self.P = P
@@ -47,6 +52,12 @@ class Grammar:
         return ', '.join([str(x) for x in [self.VN, self.VT, self.P, self.S]])
 
     def type(self) -> Type:
+        '''Returns the type of the grammar object according to the `Chomsky hierarchy`_.
+
+        If we determine the type of each production rule in the grammar,
+        then the type of the grammar will be the least restrictive type among them
+        (i.e. with the lowest type number).
+        '''
 
         def rule_type(head, tail):
             if len(head) == 1 and \
@@ -71,9 +82,11 @@ class Grammar:
         return min([rule_type(h, t) for h in self.P.keys() for t in self.P[h]])
 
     def constr_word(self) -> str:
-        '''Assuming *strictly* right-regular grammar.
+        '''Assuming a `*strictly* regular grammar
+        <https://en.wikipedia.org/wiki/Regular_grammar#Strictly_regular_grammars>`_,
+        construct a word using rules from the grammar picked at random.
 
-        :returns: A string built using rules from the grammar picked at random.
+        :returns: A random string that is valid according to the grammar.
         '''
         assert self.type() == self.Type.REGULAR
 
